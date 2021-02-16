fe7dc747f29884c19e16846e3d715d50810b6e8d
Sebastian Richards Gonzales <richardsgonzalessh@cardiff.ac.uk>
2021-02-16 15:35:02 +0000

    initial commit

---
+++ flaskblog/forms.py
@@ -0,0 +1,50 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User
+
+class RegistrationForm(FlaskForm):
+	username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+
+	email = StringField('Email', validators=[DataRequired(), Email()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+
+	submit = SubmitField('Sign Up')
+
+	def validate_username(self, username):	
+		user = User.query.filter_by(username=username.data).first()
+		if user:
+			raise ValidationError('Username taken, choose another')
+
+
+	def validate_email(self, email):
+		user = User.query.filter_by(email=email.data).first()
+		if user:
+			raise ValidationError('Email taken, choose another')
+
+
+class LoginForm(FlaskForm):
+	email = StringField('Email', validators=[DataRequired(), Email()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	submit = SubmitField('Login')
+	remember = BooleanField('Remember Me')
+
+
+class UpdateAccountForm(FlaskForm):
+	username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+
+	email = StringField('Email', validators=[DataRequired(), Email()])
+
+	picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png', 'jpeg'])])
+
+	submit = SubmitField('Update')
+
+
+class PostForm(FlaskForm):
+	title = StringField('Title', validators=[DataRequired()])
+	content = TextAreaField('Content', validators=[DataRequired()])
+	submit = SubmitField('Post')
+
